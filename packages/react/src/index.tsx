0f153223690be5b07493f897958c7094817b16b4
Natanael <nata.mw@hotmail.com>
2022-12-20 23:03:39 -0300

    :sparkles: feat: first button component storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   backgroundColor: '$ignite300',
+  fontFamily: '$default',
+  padding: '$2 $4',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Hello World</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
